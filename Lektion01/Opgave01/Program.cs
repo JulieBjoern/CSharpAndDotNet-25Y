039a0ee0af89e0131658e28eff33313859e3d0f0
Julie Bjørn <jlbb96@outlook.dk>
2026-08-24 09:43:12 +0200

    delete "TODO text"

--- Lektion01/Opgave01/Program.cs
+++ Lektion01/Opgave01/Program.cs
@@ -9,7 +9,6 @@ class Program
     {
         // Opgave 1.1: Serialiser et enkelt Item-objekt til JSON
         Item item = GetItem();
-        // TODO: Serialiser 'item' til en JSON-streng og udskriv den til konsollen
         
         string jsonString = JsonSerializer.Serialize(item);
         Console.WriteLine(jsonString);
@@ -17,7 +16,7 @@ class Program
 
         // Opgave 1.2: Serialiser et Order-objekt til JSON med pæn formatering (WriteIndented)
         Order order = GetOrder();
-        // TODO: Serialiser 'order' til en JSON-streng med WriteIndented = true og udskriv den til konsollen
+       
         var options = new JsonSerializerOptions
         {
             WriteIndented = true // Gør JSON pæn og læsbar med indrykninger
@@ -29,7 +28,6 @@ class Program
 
         // Opgave 1.3: Serialiser en liste af ordrer (List<Order>) til JSON
         List<Order> orders = GetOrders();
-        // TODO: Serialiser 'orders' til en JSON-streng og udskriv den til konsollen
 
         string jsonString2 = JsonSerializer.Serialize(orders, options);
         Console.WriteLine(jsonString2);
